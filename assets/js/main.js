bef61b8de54ca3f5dc01629aed9599386aec8c1c
donschki <donvonkrog@gmail.com>
2023-05-21 12:45:47 +0200

    a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,72 @@
 // Grab elements
 const selectElement = selector =>  {
-    const element = document.querySelector(selector)
-    if (element) return(element)
-    throw new Error(`Somehting went, make sure that ${selector} exists or is typed correctly.`)
+     const element = document.querySelector(selector)
+     if (element) return(element)
+     throw new Error(`Somehting went, make sure that ${selector} exists or is typed correctly.`)
 };
 
-//Nav styles on scroll
+
+// Nav styles on scroll
 const scrollHeader = () => {
-    const headerElement = selectElement('#header');
-    if(this.scrollY >= 100) {
-        headerElement.classList.add('activated');
-    }else {
-        headerElement.classList.remove('activated');
-    };
+     const headerElement = selectElement('#header');
+     if(this.scrollY >= 100 || this.scrollY >= 1) {
+         headerElement.classList.add('activated');
+         
+     } else {
+         headerElement.classList.remove('activated');
+     };
 };
+
 window.addEventListener('scroll', scrollHeader);
+
 
 // Open menu & search pop-up
 const menuToggleIcon = selectElement('#menu-toggle-icon');
-const toggleMenu = () => {
-    const mobileMenu = selectElement('#menu');
-    mobileMenu.classList.toggle('activated');
-    menuToggleIcon.classList.toggle('activated');
-};
+
+     const toggleMenu = () => {
+         const mobileMenu = selectElement('#menu');
+         mobileMenu.classList.toggle('activated');
+         menuToggleIcon.classList.toggle('activated');
+     };
+
 menuToggleIcon.addEventListener('click', toggleMenu)
 
-// Open/Close search form popup
 
-// -- Close the search form popup on ESC keypress
+// Close the search form popup on ESC keypress
+window.addEventListener ('keyup', event=>{
+    if(event.key === 'Escape') searchFromContainer.classList.remove('activated')
+})
 
 // Switch theme/add to local storage
 const bodyElement = document.body;
 const themeToggleBtn = selectElement('#theme-toggle-btn');
 const currentTheme = localStorage.getItem('currentTheme');
 
-if(currentTheme){
-    bodyElement.classList.add('light-theme');
-}
+     if(currentTheme){
+         bodyElement.classList.add('light-theme');
+     }
+     
+     themeToggleBtn.addEventListener('click', () => {
+         bodyElement.classList.toggle('light-theme');
+     
+         if (bodyElement.classList.contains('light-theme')){
+             localStorage.setItem('currentTheme', 'themeActive');
 
-themeToggleBtn.addEventListener('click', () => {
-    bodyElement.classList.toggle('light-theme');
+             } else {
+                 localStorage.removeItem('themeActive');
+             };
+         
+     });
 
-     if (
-        bodyElement.classList.contains('light-theme')){
-        localStorage.setItem('currentTheme', 'themeActive')
-        }
-        else {
-            localStorage.removeItem('currentTheme',)
-        };
+     
+// Open/Close search form popup
+const formOpenBtn = selectElement('#search-icon');
+const formCloseBtn= selectElement('#form-close-btn');
+const searchFromContainer = selectElement('#search-form-container');
 
-});
+formOpenBtn.addEventListener ('click', () => searchFromContainer.classList.add('activated'))
+
+formCloseBtn.addEventListener ('click', () => searchFromContainer.classList.remove('activated'))
+
+
 // Swiper
